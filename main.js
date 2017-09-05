ba763406fe466cef898e409a6464c44accef49a3
NekoTony <cheesezwheel@outlook.com>
2017-09-04 20:05:50 -0400

    Update

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const ipcMain = require('electron').ipcMain;
 var app = require('app');
 globalShortcut = require('global-shortcut')
 var BrowserWindow = require('browser-window');  
-
-
-var mainWindow = null;
+const electron = require('electron');
+const dialog = electron.dialog;
 
 app.commandLine.appendSwitch('ppapi-flash-path', '/usr/lib/adobe-flashplugin/libpepflashplayer.so');
 
@@ -14,6 +13,9 @@ require('ipc').on('load-page', (event, arg) => {
     mainWindow.loadURL(arg);
 });
 
+dialog.showErrorBox = function(title, content) {
+    console.log(`${title}\n${content}`);
+};
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
@@ -26,6 +28,7 @@ app.on('ready', function() {
     'width': 800,
     'height': 600,
     'frame' : false,
+    'icon': __dirname + '/icon.png',
     'web-preferences': {
       'plugins': true,
       'sandbox' : true
