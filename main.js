9274c5174888dae71492c76d6a5dda544c1ba288
NekoTony <cheesezwheel@outlook.com>
2017-09-07 18:59:01 -0400

    Fix Bug/Non DRagale

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const ipcMain = require('electron').ipcMain;
 var app = require('app');
 var path = require('path');
 globalShortcut = require('global-shortcut')
-var BrowserWindow = require('browser-window');  
+var BrowserWindow = require('browser-window');
 const electron = require('electron');
 const dialog = electron.dialog;
 var flash_path = path.join(__dirname, 'flash-plugin/libpepflashplayer.so');
@@ -22,45 +22,45 @@ dialog.showErrorBox = function(title, content) {
 };
 
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({
-    'width': 800,
-    'height': 600,
-    'frame' : false,
-    'minWidth' : 800,
-    'minHeight' : 600,
-    icon: path.join(__dirname, 'icon.png'),
-    'web-preferences': {
-      'plugins': true,
-      'sandbox' : true
-    }
-  });
-  globalShortcut.register('Ctrl+e', () => {
-    var window = BrowserWindow.getFocusedWindow();
-    window.close();
-  });
-  
-  globalShortcut.register('Ctrl+m', () => {
-    var window = BrowserWindow.getFocusedWindow();
-    if(window.isMaximized()){
-        window.unmaximize();
-    }else{
-        window.maximize();
-      }
-  });
-  
-  globalShortcut.register('Ctrl+a', () => {
-      mainWindow.loadURL('http://atelier801.com/forums');
-  });
+    mainWindow = new BrowserWindow({
+        'width': 800,
+        'height': 600,
+        'frame': false,
+        'minWidth': 800,
+        'minHeight': 600,
+        icon: path.join(__dirname, 'icon.png'),
+        'web-preferences': {
+            'plugins': true,
+            'sandbox': true
+        }
+    });
+    globalShortcut.register('Ctrl+e', () => {
+        var window = BrowserWindow.getFocusedWindow();
+        window.close();
+    });
 
-  globalShortcut.register('Ctrl+t', () => {
-      mainWindow.loadURL('http://www.transformice.com/TransformiceChargeur.swf');
-  });
+    globalShortcut.register('Ctrl+m', () => {
+        var window = BrowserWindow.getFocusedWindow();
+        if (window.isMaximized()) {
+            window.unmaximize();
+        } else {
+            window.maximize();
+        }
+    });
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+    globalShortcut.register('Ctrl+a', () => {
+        mainWindow.loadURL('http://atelier801.com/forums');
+    });
+
+    globalShortcut.register('Ctrl+t', () => {
+        mainWindow.loadURL('http://www.transformice.com/TransformiceChargeur.swf');
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
